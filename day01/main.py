cb7e237052435fe58db83f63cce2eec2d1c1c1c1
Floozutter <me@floozutter.site>
2024-12-01 15:04:08 -0800

    add day01

--- day01/main.py
+++ day01/main.py
@@ -1,4 +1,12 @@
+from collections import Counter
+
 INPUTPATH = "input.txt"
 #INPUTPATH = "input-test.txt"
 with open(INPUTPATH) as ifile:
     raw = ifile.read()
+l, r = zip(*(map(int, line.split()) for line in raw.strip().splitlines()))
+
+print(sum(abs(b-a) for a, b in zip(sorted(l), sorted(r))))
+
+counts = Counter(r)
+print(sum(a*counts[a] for a in l))
