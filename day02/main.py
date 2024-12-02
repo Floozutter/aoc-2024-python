71098555f0f98b1bf77beb33b38d3fb0bee79285
Floozutter <me@floozutter.site>
2024-12-02 14:31:09 -0800

    add day02

--- day02/main.py
+++ day02/main.py
@@ -2,3 +2,11 @@ INPUTPATH = "input.txt"
 #INPUTPATH = "input-test.txt"
 with open(INPUTPATH) as ifile:
     raw = ifile.read()
+reports = tuple(tuple(map(int, line.split())) for line in raw.strip().splitlines())
+
+def safe(report: tuple[int, ...]) -> bool:
+    pairs = tuple(zip(report, report[1:]))
+    return all(a < b and b-a < 4 for a, b in pairs) or all(a > b and a-b < 4 for a, b in pairs)
+
+print(sum(map(safe, reports)))
+print(sum(safe(r) or any(safe(r[:i]+r[i+1:]) for i in range(len(r))) for r in reports))
